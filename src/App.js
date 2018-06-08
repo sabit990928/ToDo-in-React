581ab341c38f6d630bd875f4b4a8648ddbb6ba78
sabit990928 <sabit990928@gmail.com>
2018-06-08 12:56:33 +0600

    without Redux and delete

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Row, Col, Button } from 'antd';
+
+import './App.css';
+
+class App extends Component {
+  state = { task: '', list: [] };
+
+
+  onAddPress = () => {
+    const helpList = [...this.state.list, this.state.task];
+    this.setState({ list: helpList, task: '' });
+    console.log(this.state);
+  }
+
+  handleChange = (event) => {
+    const { target: { value } } = event;
+    this.setState({ task: value });
+  };
+
+  // delete = (id) => {
+  //   this.setState(prevState => ({
+  //     list: prevState.list.filter(el => el !== id),
+  //   }));
+  // }
+
+  render() {
+  //   const todoNode = this.state.list.map(todo => todo);
+    return (
+
+      <div className="App">
+        <div id="app" />
+        <div className="container">
+          <h1>Todo</h1>
+          <form>
+            <div>
+              Add a new task
+              <br />
+              <input
+                type="text"
+                name="task"
+                placeholder="I want to..."
+                value={this.state.task}
+                onChange={this.handleChange}
+              />
+              <Button className="button" onClick={this.onAddPress}>Add</Button>
+            </div>
+          </form>
+          <ol>
+            {
+              this.state.list.length > 0 &&
+              this.state.list.map(task => <li key={task.toString()} onClick={this.didIt}>{task}</li>)
+            }
+          </ol>
+          <form>
+            <input
+              type="checkbox"
+              value={this.state.list}
+            />
+          </form>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/TaskList.js
@@ -0,0 +1,16 @@
+// import React, { Component } from 'react';
+
+// class TaskList extends Component {
+//   render() {
+//     const listItems = this.props.list.map(number => <li>{number}</li>);
+//     return (
+//       <div className="ListItems">
+//         <ul>
+//           {listItems}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// export default TaskList;
