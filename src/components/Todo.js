f74c028e9708e1a028b6f72c99ecb57829ff5dfe
sabit990928 <sabit990928@gmail.com>
2018-06-13 15:20:26 +0600

    feat: introduce Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const LineThrough = styled.span`
+${({ isThrough }) => (isThrough ? 'text-decoration: line-through' : 'text-decoration: none')};
+`;
 
-const Todo = ({
-  title, isCompleted, onCompleteButtonClick, id,
-}) => (
-// }) => console.log('TodoRender: ', title) || (
+const Todo = ({ todo: { id, isCompleted, title }, onClick }) => {
+  const handleItemClick = ({ target: { checked } }) =>
+    onClick({ id, checked });
 
-  <li key={id}>
-    {title}
-    <button onClick={onCompleteButtonClick}>
-      COMPLETED
-    </button>
-  </li>
+  return (
+    <div key={id}>
 
-);
+      <input type="checkbox" name="isCompleted" checked={isCompleted} onChange={handleItemClick} />
+      <LineThrough isThrough={isCompleted}>{title}</LineThrough>
+    </div>
+  );
+};
 
 export default Todo;
