a83b7861c27177b69f2bc05d7c88b234601fa5fa
sabit990928 <sabit990928@gmail.com>
2018-06-13 17:34:53 +0600

    style(TodoForm): include antd Input and Form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { func } from 'prop-types';
-import { Input } from 'antd';
+import { Input, Form, Button } from 'antd';
 
 class TodoForm extends PureComponent {
   static propTypes = {
@@ -22,20 +22,24 @@ class TodoForm extends PureComponent {
   }
 
   render() {
-    // console.log('TodoForm render');
+    const FormItem = Form.Item;
     return (
-      <form onSubmit={this.handleSubmitButtonClick}>
-        <div>Add a new task</div>
+      <Form layout="inline" onSubmit={this.handleSubmitButtonClick}>
         <br />
-        <Input
-          size="large"
-          name="task"
-          placeholder="I want to..."
-          value={this.state.value}
-          onChange={this.handleInputChange}
-        />
-        <button type="submit" className="button">Add</button>
-      </form>
+        <FormItem>
+          <Input
+            type="text"
+            size="large"
+            name="task"
+            placeholder="I want to..."
+            value={this.state.value}
+            onChange={this.handleInputChange}
+          />
+        </FormItem>
+        <FormItem>
+          <Button type="primary" htmlType="submit" size="large">Add</Button>
+        </FormItem>
+      </Form>
     );
   }
 }
