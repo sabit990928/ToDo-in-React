e38f94ecdf2d763b3c40f085820dd5d9de214be6
sabit990928 <sabit990928@gmail.com>
2018-06-15 17:51:20 +0600

    feat(Todo):  fix List to antd

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Checkbox } from 'antd';
+import { Checkbox, List } from 'antd';
 
 const LineThrough = styled.span`
 ${({ isThrough }) => (isThrough ? 'text-decoration: line-through' : 'text-decoration: none')};
 `;
-
+const StyledList = styled(List)`
+  width: 100%;
+`;
 const Todo = ({ todo: { id, isCompleted, title }, onClick }) => {
   const handleItemClick = ({ target: { checked } }) =>
     onClick({ id, checked });
 
   return (
-    <div key={id}>
-      <Checkbox name="isCompleted" checked={isCompleted} onChange={handleItemClick}>
-        <LineThrough isThrough={isCompleted}>{title}</LineThrough>
-      </Checkbox>
-    </div>
+    <StyledList
+      key={id}
+      size="small"
+      bordered
+      itemLayout="horizontal"
+    >
+      <List.Item>
+        <Checkbox name="isCompleted" checked={isCompleted} onChange={handleItemClick}>
+          <LineThrough isThrough={isCompleted}>{title}</LineThrough>
+        </Checkbox>
+      </List.Item>
+    </StyledList>
   );
 };
-
 export default Todo;
