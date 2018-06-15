d0034cc3dfdd66c78634e604f96b30ad43f2425b
sabit990928 <sabit990928@gmail.com>
2018-06-15 17:52:47 +0600

    style(TodoFilter): Change Button to primary

--- src/components/TodoFilter.js
+++ src/components/TodoFilter.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { func } from 'prop-types';
+import { func, any } from 'prop-types';
 import { Button } from 'antd';
 
-const TodoFilter = ({ onTypeChange }) => (
+const TodoFilter = ({ onTypeChange, currentFilter }) => (
   <div>
-    <Button type="primary" name="all" onClick={onTypeChange}>All</Button>
-    <Button name="active" onClick={onTypeChange}>Active</Button>
-    <Button name="completed" onClick={onTypeChange}>Completed</Button>
+    <Button type={currentFilter === 'all' || !currentFilter ? 'primary' : 'normal'} name="all" onClick={onTypeChange}>All</Button>
+    <Button type={currentFilter === 'active' ? 'primary' : 'normal'} name="active" onClick={onTypeChange}>Active</Button>
+    <Button type={currentFilter === 'completed' ? 'primary' : 'normal'} name="completed" onClick={onTypeChange}>Completed</Button>
   </div>
 );
-
+/* eslint-disable react/forbid-prop-types */
 TodoFilter.propTypes = {
   onTypeChange: func.isRequired,
+  currentFilter: any,
+};
+/* eslint-enable react/forbid-prop-types */
+
+TodoFilter.defaultProps = {
+  currentFilter: null,
 };
 
 export default TodoFilter;
