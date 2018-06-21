3db13085d0bf1969045d66388e3f0fc88b1ff9ff
sabit990928 <sabit990928@gmail.com>
2018-06-21 15:28:01 +0600

    feat(AddTodoActions): making array  with id, title, isCompleted

--- src/actions/AddTodoActions.js
+++ src/actions/AddTodoActions.js
@@ -1,15 +1,20 @@
 import uuidv1 from 'uuid';
 
-import { TODO_INPUT_CHANGED, ADD_TODO } from './types';
-
-const id = uuidv1();
+import { TODO_INPUT_CHANGED, ADD_TODO, TODO_INPUT_CLEAN } from './types';
 
 export const todoInputChanged = text => ({
   type: TODO_INPUT_CHANGED,
   payload: text,
 });
 
+export const todoInputClean = text => ({
+  type: TODO_INPUT_CLEAN,
+  payload: text,
+});
+
 export const addTodo = todo => ({
   type: ADD_TODO,
-  payload: todo,
+  id: uuidv1(),
+  todo,
+  isCompleted: false,
 });
