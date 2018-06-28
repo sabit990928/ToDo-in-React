d70fd4b58fae811a5dcda3f336cd70bfaa364188
sabit990928 <sabit990928@gmail.com>
2018-06-28 16:11:12 +0600

    feat(App): connect to Firebase added privateRoute for TodoPage and public Route for LoginPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
+import firebase from 'firebase';
 
 import { LoginPage, TodoPage } from './components';
 
@@ -7,25 +8,53 @@ import './App.css';
 
 const appRoutes = [
   {
-    path: '/login',
-    component: LoginPage,
-    render: props => <LoginPage {...props} />,
+    path: '/todo',
+    component: TodoPage,
+    render: props => <TodoPage {...props} />,
   },
-  // {
-  //   path: '/Todo',
-  //   component: TodoPage,
-  //   render: props => <App {...props} />,
-  // },
 ];
 
+const PrivateRoute = ({ component: Component, isUserAuth, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (isUserAuth
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />)}
+  />
+);
+
+const AuthRoute = ({ component: Component, isUserAuth, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (!isUserAuth
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/', state: { from: props.location } }} />)}
+  />
+);
+
 class App extends PureComponent {
+  componentDidMount() {
+    const config = {
+      apiKey: 'AIzaSyCqmfoHGRfPV0oJjQ7qWv5ZVm99PDjOBbA',
+      authDomain: 'todo-d3c6c.firebaseapp.com',
+      databaseURL: 'https://todo-d3c6c.firebaseio.com',
+      projectId: 'todo-d3c6c',
+      storageBucket: 'todo-d3c6c.appspot.com',
+      messagingSenderId: '336941408236',
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <div className="App">
         <BrowserRouter>
           <Switch>
+            <Route path="/" exact component={() => <div>Home Page</div>} />
+            <AuthRoute path="/login" exact component={LoginPage} isUserAuth />
+            <AuthRoute path="/register" exact component={() => <div>Sign up Page</div>} isUserAuth />
             {appRoutes.map(route => (
-              <Route
+              <PrivateRoute
+                isUserAuth
                 exact
                 key={route.path}
                 path={route.path}
